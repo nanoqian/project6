33b385dfe41549e574cf48a1e0f9b1298bb38f69
quailcoral <64557571+quailcoral@users.noreply.github.com>
2025-02-09 11:48:33 -0500

    Tokenize brackets, constants, and operations

--- project6.test.ts
+++ project6.test.ts
@@ -1,5 +1,100 @@
 import * as p6 from "../project6";
 
-test ("test", () => {
-    expect(p6.tokenize("")).toBe("not implemented");
+describe("tokenize", () => {
+    test ("empty is empty", () => {
+        expect(p6.tokenize("")).toEqual([]);
+    });
+
+    test("brackets are brackets", () => {
+        const brackets: p6.Bracket[] = ["(", ")", "[", "]", "{", "}"];
+        brackets.map((bracket) => {
+            expect(p6.isBracket(bracket)).toEqual(p6.tagBracket(bracket));
+        });
+    });
+
+    test("non-brackets aren't brackets", () => {
+        ["a", "1", "()", "*", "<"].map((bracket) => {
+            expect(p6.isBracket(bracket)).toBeUndefined();
+        });
+    });
+
+    test("brackets tokenize", () => {
+        expect(p6.tokenize("[{)")).toEqual(["[", "{", ")"].map(p6.tagBracket));
+    });
+
+    test("binary operations are operations", () => {
+        const operations: p6.BinaryOperation[] = ["+", "-", "*", "/", "^"];
+        operations.map((operation) => {
+            expect(p6.isOperation(operation)).toEqual(p6.tagOperation(operation));
+        });
+    });
+
+    test ("brackets and binary operations tokenize", () => {
+        expect(p6.tokenize("[+-}}//[[")).toEqual([
+            p6.tagBracket("["), p6.tagOperation("+"), p6.tagOperation("-"),
+            p6.tagBracket("}"), p6.tagBracket("}"), p6.tagOperation("/"),
+            p6.tagOperation("/"), p6.tagBracket("["), p6.tagBracket("[")
+        ]);
+    });
+
+    test("unary operations are operations", () => {
+        const operations: p6.UnaryOperation[] =
+            ["-", "sqrt", "log", "ln", "!", "sin", "cos", "tan", "sec", "csc", "cot"]
+        operations.map((operation) => {
+            expect(p6.isOperation(operation)).toEqual(p6.tagOperation(operation));
+        });
+    });
+
+    test("non-operations aren't operations", () => {
+        ["(", ")", "++", "[", "]", "{", "}", "l", "sqr"].map((operation) => {
+            expect(p6.isOperation(operation)).toBeUndefined();
+        });
+    });
+
+    test("unary operations tokenize", () => {
+        expect(p6.tokenize("-sqrtsin!")).toEqual(["-", "sqrt", "sin", "!"].map(p6.tagOperation));
+    });
+
+    test("constants are constants", () => {
+        ["pi", "e"].map((constant) => {
+            expect(p6.isConstant(constant)).toEqual(p6.tagConstant(constant as p6.Constant));
+        });
+    });
+
+    test("non-constants aren't constants", () => {
+        ["(", ")", "[", "]", "ee", "pie", "+", "l", "sqrt"].map((constant) => {
+            expect(p6.isConstant(constant)).toBeUndefined();
+        });
+    });
+
+    test("constants tokenize", () => {
+        expect(p6.tokenize("piepie")).toEqual(["pi", "e", "pi", "e"].map(p6.tagConstant));
+    });
+
+    test("whitespace isempty", () => {
+        expect(p6.tokenize("     ")).toEqual([]);
+    });
+
+    test("whitespace between operations ignored", () => {
+        ["pi sqrt++", "(  sincos  ^ [ln", "   sqrt ee+e  "].map((input) => {
+            const input_no_whitespace = input.replace(/ /g, "");
+            expect(p6.tokenize(input)).toEqual(p6.tokenize(input_no_whitespace));
+        });
+    });
+
+    test("whitespace inside operations not ignored", () => {
+        ["sq  rt", "(  si ncos  ^ [l n", "   sqr t ee+e  "].map((input) => {
+            const input_no_whitespace = input.replace(/ /g, "");
+            expect(p6.tokenize(input)).not.toEqual(p6.tokenize(input_no_whitespace));
+        });
+    });
+
+    test("brackets, constants, and operations tokenize", () => {
+        expect(p6.tokenize(" ][  sin  cos lnpi]-]++ { ")).toEqual([
+            p6.tagBracket("]"), p6.tagBracket("["), p6.tagOperation("sin"),
+            p6.tagOperation("cos"), p6.tagOperation("ln"), p6.tagConstant("pi"),
+            p6.tagBracket("]"), p6.tagOperation("-"), p6.tagBracket("]"),
+            p6.tagOperation("+"), p6.tagOperation("+"), p6.tagBracket("{")
+        ]);
+    });
 });
--- project6.ts
+++ project6.ts
@@ -8,9 +8,25 @@ export type UnaryOperation =
 export type BinaryOperation =
     | "+" | "-" | "*" | "/" | "^"
 
-export type Token =
-    | number | Constant | UnaryOperation | BinaryOperation
+export type Operation =
+    | UnaryOperation | BinaryOperation
+
+export type Bracket =
     | "(" | ")" | "[" | "]" | "{" | "}"
+
+export type TokenUnion =
+    | number | Bracket | Constant | Operation
+
+export interface Tag<T extends TokenUnion> {
+    kind: T extends number ? "number"
+          : T extends Bracket ? "bracket"
+          : T extends Constant ? "constant"
+          : T extends Operation ? "operation"
+          : never;
+    value: T;
+}
+
+export type Token = Tag<TokenUnion>
 
 export type Expression =
     | { kind: "number"; value: number }
@@ -18,8 +34,85 @@ export type Expression =
     | { kind: "unary"; operation: UnaryOperation; operand: Expression }
     | { kind: "binary"; operation: BinaryOperation; operand1: Expression; operand2: Expression }
 
+export function tagNumber(value: number): Tag<number> {
+    return { kind: "number", value };
+}
+
+export function tagBracket(value: Bracket): Tag<Bracket> {
+    return { kind: "bracket", value };
+}
+
+export function tagConstant(value: Constant): Tag<Constant> {
+    return { kind: "constant", value };
+}
+
+export function tagOperation(value: Operation): Tag<Operation> {
+    return { kind: "operation", value }
+}
+
+export function isBracket(input: string): Tag<Bracket> | undefined {
+    switch (input) {
+        case "(":
+        case ")":
+        case "[":
+        case "]":
+        case "{":
+        case "}":
+            return tagBracket(input);
+        default:
+            return;
+    }
+}
+
+export function isConstant(input: string): Tag<Constant> | undefined {
+    switch (input) {
+        case "pi":
+        case "e":
+            return tagConstant(input);
+        default:
+            return;
+    }
+}
+
+export function isOperation(input: string): Tag<Operation> | undefined {
+    switch (input) {
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+        case "^":
+        case "sqrt":
+        case "log":
+        case "ln":
+        case "!":
+        case "sin":
+        case "cos":
+        case "tan":
+        case "sec":
+        case "csc":
+        case "cot":
+            return tagOperation(input);
+        default:
+            return;
+    }
+}
+
 export function tokenize(input: string): Token[] | string {
-    return "not implemented";
+    let tokens = [];
+    let chunk = "";
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === " ") {
+            chunk = "";
+        } else {
+            chunk = chunk.concat(input[i]);
+            const token = isBracket(chunk) ?? isConstant(chunk) ?? isOperation(chunk);
+            if (token !== undefined) {
+                tokens.push(token);
+                chunk = "";
+            }
+        }
+    }
+    return tokens;
 }
 
 export function parse(input: Token[]): Expression | string {
