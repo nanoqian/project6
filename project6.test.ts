7c06dc4ea0835f55d402a4c3209480104c8e8971
quailcoral <64557571+quailcoral@users.noreply.github.com>
2025-02-11 19:14:20 -0500

    Tokenize numbers

--- project6.test.ts
+++ project6.test.ts
@@ -71,7 +71,7 @@ describe("tokenize", () => {
         expect(p6.tokenize("piepie")).toEqual(["pi", "e", "pi", "e"].map(p6.tagConstant));
     });
 
-    test("whitespace isempty", () => {
+    test("whitespace is empty", () => {
         expect(p6.tokenize("     ")).toEqual([]);
     });
 
@@ -96,5 +96,49 @@ describe("tokenize", () => {
             p6.tagBracket("]"), p6.tagOperation("-"), p6.tagBracket("]"),
             p6.tagOperation("+"), p6.tagOperation("+"), p6.tagBracket("{")
         ]);
+    });
+
+    test("digits are digits", () => {
+        const digits: p6.Digit[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        digits.map((digit) => {
+            expect(p6.isDigit(digit)).toEqual(p6.tagDigit(digit));
+        });
+    });
+
+    test("non-digits are digits", () => {
+        ["00", "1a", "234", "*", "/", "sqrt", "654", "77", "a8", ""].map((digit) => {
+            expect(p6.isDigit(digit)).toBeUndefined();
+        });
+    });
+
+    test("digits concatenate", () => {
+        const lists: p6.Digit[][] = [["0", "1", "2"], ["5", "0", "0"], ["0", "0"]];
+        lists.map((digits) => {
+            expect(p6.concatDigits(digits.map(p6.tagDigit))).toBe(digits.join(""));
+        });
+    });
+
+    test("integers tokenize", () => {
+        expect(p6.tokenize("1234 033 00009")).toEqual([1234, 33, 9].map(p6.tagNumber));
+    });
+
+    test("decimals tokenize", () => {
+        expect(p6.tokenize("12.034 .03300 010.")).toEqual([12.034, 0.033, 10].map(p6.tagNumber));
+    });
+
+    test("lone period errors", () => {
+        expect(p6.tokenize("55.0. 00")).toBe("\".\" not a valid input");
+    });
+
+    test("numbers tokenize with everything else", () => {
+        expect(p6.tokenize(".5sincos    pi100.e sqrt56   .5")).toEqual([
+            p6.tagNumber(0.5), p6.tagOperation("sin"), p6.tagOperation("cos"),
+            p6.tagConstant("pi"), p6.tagNumber(100), p6.tagConstant("e"),
+            p6.tagOperation("sqrt"), p6.tagNumber(56), p6.tagNumber(0.5)
+        ]);
+    });
+
+    test("non-names error", () => {
+        expect(p6.tokenize("sqrthi")).toBe("\"hi\" not a valid input");
     });
 });
--- project6.ts
+++ project6.ts
@@ -14,11 +14,15 @@ export type Operation =
 export type Bracket =
     | "(" | ")" | "[" | "]" | "{" | "}"
 
+export type Digit =
+    | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+
 export type TokenUnion =
     | number | Bracket | Constant | Operation
 
-export interface Tag<T extends TokenUnion> {
+export interface Tag<T extends TokenUnion | Digit> {
     kind: T extends number ? "number"
+          : T extends Digit ? "digit"
           : T extends Bracket ? "bracket"
           : T extends Constant ? "constant"
           : T extends Operation ? "operation"
@@ -36,6 +40,10 @@ export type Expression =
 
 export function tagNumber(value: number): Tag<number> {
     return { kind: "number", value };
+}
+
+export function tagDigit(value: Digit): Tag<Digit> {
+    return { kind: "digit", value };
 }
 
 export function tagBracket(value: Bracket): Tag<Bracket> {
@@ -97,22 +105,113 @@ export function isOperation(input: string): Tag<Operation> | undefined {
     }
 }
 
-export function tokenize(input: string): Token[] | string {
-    let tokens = [];
-    let chunk = "";
-    for (let i = 0; i < input.length; i++) {
-        if (input[i] === " ") {
-            chunk = "";
-        } else {
-            chunk = chunk.concat(input[i]);
-            const token = isBracket(chunk) ?? isConstant(chunk) ?? isOperation(chunk);
-            if (token !== undefined) {
-                tokens.push(token);
-                chunk = "";
+export function isDigit(input: string): Tag<Digit> | undefined {
+    switch (input) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            return tagDigit(input);
+        default:
+            return;
+    }
+}
+
+export function concatDigits(digits: Tag<Digit>[]): string {
+    return digits.map((digit) => digit.value).join("");
+}
+
+class Tokenizer {
+    readonly input: string = "";
+    index: number = 0;
+
+    constructor(input: string) {
+        this.input = input;
+    }
+
+    run(): Token[] | string {
+        let tokens = [];
+        while (this.index < this.input.length) {
+            const char = this.input[this.index];
+            if (char === " ") {
+                this.index++;
+            } else if (isDigit(char) || char === ".") {
+                const number = this.matchNumber();
+                if (number !== undefined) {
+                    if (typeof number === "string") {
+                        return number;
+                    } else {
+                        tokens.push(number);
+                    }
+                }
+            } else {
+                const token = this.matchName();
+                if (token !== undefined) {
+                    if (typeof token === "string") {
+                        return token;
+                    } else {
+                        tokens.push(token);
+                    }
+                }
             }
         }
+        return tokens;
     }
-    return tokens;
+
+    matchName(): Tag<Bracket | Constant | Operation> | string {
+        let chunk = "";
+        while (this.index < this.input.length) {
+            chunk = chunk.concat(this.input[this.index]);
+            if (chunk.length > 4) {
+                break;
+            }
+            this.index++;
+
+            const token = isBracket(chunk) ?? isConstant(chunk) ?? isOperation(chunk);
+            if (token !== undefined) {
+                return token;
+            }
+        }
+        return "\"" + chunk + "\" not a valid input";
+    }
+
+    matchInteger(): Tag<Digit>[] {
+        let digits: Tag<Digit>[] = [];
+        while (this.index < this.input.length) {
+            const digit = isDigit(this.input[this.index]);
+            if (digit !== undefined) {
+                digits.push(digit);
+                this.index++;
+            } else {
+                break;
+            }
+        }
+        return digits;
+    }
+
+    matchNumber(): Tag<number> | string {
+        const integer = this.matchInteger();
+        let decimal = [];
+        if (this.input[this.index] == ".") {
+            this.index++;
+            decimal = this.matchInteger();
+        }
+        if (integer.length !== 0 || decimal.length !== 0) {
+            return tagNumber(parseFloat(concatDigits(integer) + "." + (concatDigits(decimal))));
+        } else {
+            return "\".\" not a valid input";
+        }
+    }
+}
+
+export function tokenize(input: string): Token[] | string {
+    return new Tokenizer(input).run();
 }
 
 export function parse(input: Token[]): Expression | string {
